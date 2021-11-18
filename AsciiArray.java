e768f0c4ff3e00eb6a1b178154eafbf19f18cdf2
Rajat Mishra <94281732+RajatMishra21@users.noreply.github.com>
2021-11-18 21:48:24 +0530

    Add files via upload

---
+++ AsciiArray.java
@@ -0,0 +1,7 @@
+public class AsciiArray {
+    public static void main(String[] args) {
+        int[] arr = {73,76, 111, 118, 101, 74,97,118,97};
+        for (int a: arr)
+            System.out.print((char)a + " ");
+    }
+}
